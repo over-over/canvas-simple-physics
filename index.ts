279a2b6ad9d2d173de5273106eeee44db1d2d77d
over-over <31822451+over-over@users.noreply.github.com>
2022-10-03 17:27:32 +0400

    added utils

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Import stylesheets
+import { runDemo } from './src/demo';
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+runDemo();
